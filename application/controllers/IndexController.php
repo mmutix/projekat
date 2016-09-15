080f0c91edb3fa05bbba66317ee8afe0987ac67e
gisadmin <gisadmin@GIS1-58>
2016-09-15 22:07:20 +0200

    1509 IndexPage

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -20,8 +20,54 @@ class IndexController extends Zend_Controller_Action
 				'order_number' => 'ASC'
 			)
 		));
-		
+                
+                $cmsSitemapPagesDbTable = new Application_Model_DbTable_CmsSitemapPages();
+		$servicesSitemapPages = $cmsSitemapPagesDbTable->search(array(
+                'filters' => array(
+                'status' => Application_Model_DbTable_CmsSitemapPages::STATUS_ENABLED,
+                'type' => 'ServicesPage'
+			),
+                'limit' => 1
+		));
+                $newsSitemapPages = $cmsSitemapPagesDbTable->search(array(
+                    'filters' => array(
+                    'status' => Application_Model_DbTable_CmsSitemapPages::STATUS_ENABLED,
+                    'type' => 'NewsPage',
+                                ),
+                    'limit' => 1
+                        ));
+                $servicesSitemapPage = !empty($servicesSitemapPages) ? $servicesSitemapPages[0] : null;
+        
+                $cmsServicesDbTable = new Application_Model_DbTable_CmsServices();
+                $newsSitemapPage = !empty($newsSitemapPages) ? $newsSitemapPages[0] : null;
+                $cmsNewsDbTable = new Application_Model_DbTable_CmsNews();
+                $news = $cmsNewsDbTable->search(array(
+                    'filters' => array(
+                        'status'=>  Application_Model_DbTable_CmsNews::STATUS_ENABLED,
+                    ),
+                    'orders' => array(//sortiram tabelu po
+                        'order_number'=>'ASC'
+                    ),
+                    'limit' => 9,
+                    //'page' => 2
+                ));
+                
+		$services = $cmsServicesDbTable->search(array(
+                'filters' => array(
+                'status'=>  Application_Model_DbTable_CmsServices::STATUS_ENABLED,
+                ),
+                'orders' => array(
+                'order_number'=>'ASC'
+                ),
+                'limit' => 4,
+                //'page' => 2
+                ));
+                
 		$this->view->indexSlides = $indexSlides;
+                $this->view->news = $news;
+                $this->view->newsSitemapPage = $newsSitemapPage;
+                $this->view->services = $services;
+                $this->view->servicesSitemapPage = $servicesSitemapPage;
     }
 }
 
